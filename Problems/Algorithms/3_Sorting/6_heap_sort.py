7108d3e1e389d993fdebef74d75a47c6d5a5e14f
Suhas Srivats Subburathinam <subburat@synopsys.com>
2020-04-26 14:20:39 -0700

    docs: add time and space complexity info

--- Problems/Algorithms/3_Sorting/6_heap_sort.py
+++ Problems/Algorithms/3_Sorting/6_heap_sort.py
@@ -1,4 +1,48 @@
+"""
+Time Complexity:
+
+1) Building a max-heap takes O(n)
+Source: https://www.youtube.com/watch?v=XZ_Beap6Vo0
+Explanation:
+If array has n elements, max nodes at height h is (n/(2^h))
+Time complexity for one level is h * num_of_nodes_at_that_level = `h*(n/(2^h))`
+Total number of levels in BinaryTree is log(n) to base 2.
+
+Therefore, total time complexity is:
+=> ∑(0->logn) n/(2^h) * O(h)
+=> O(n ∑(0->logn) h/(2^h))
+=> O(n ∑(0->infinity) h/(2^h)) //upper bound; ∑(0->infinity) h/(2^h) = 2 (wiki)
+=> O(n * 2)
+=> O(n)
+
+2) Heapify method takes time complexity of O(n logn) for n nodes
+Source: http://www.cs.toronto.edu/~krueger/cscB63h/lectures/tut02.txt
+-MaxHeapify is recursively called **at most** h times, where h is the
+ height of the subtree starting at i
+-Each call to MaxHeapify takes c steps, for some constant c
+
+Therefore:
+for all inputs, **at most** ch steps are needed where h is the
+ height of the subtree starting at i
+
+So, the worst case time complexity is O(h) where h is the subtree with
+root i OR O(log n) since h = log n = the height of the entire tree
+
+For n nodes, n * O(h) => O(n logn)
+
+3) Space Complexity: O(1)
+"""
+
+
 def heapify(arr, n, i):
+    """
+    :param: arr - array to heapify
+    n -- number of elements in the array
+    i -- index of the current node
+    TODO: Converts an array (in place) into a maxheap, a complete binary tree
+    with the largest values at the top
+    """
+
     # Using i as the index of the current node, find the 2 child nodes
     # (if the array were a binary tree) and find the largest value. If one of
     # the children is larger swap the values and recurse into that subree
@@ -26,7 +70,8 @@ def heapify(arr, n, i):
 def heapsort(arr):
     n = len(arr)
 
-    # As a first step, convert the array to max-heap
+    # As a first step, convert the array to max-heap. In this step, we are
+    # building a max-heap
     for i in range(n, -1, -1):
         heapify(arr, n, i)
     # print(arr)
@@ -34,7 +79,6 @@ def heapsort(arr):
     # Extract elements one-by-one. Remember, we remove a node from root node
     # but this may violate the properties of a Heap. Therefore, we need
     # to heapify
-
     for i in range(n-1, 0, -1):
         # Swap last element with root node (in otherwords, remove root node)
         arr[i], arr[0] = arr[0], arr[i]
